6a0cb22b5fb1b6099cf5005b1de49295d06e2aa3
Oleg Voloshyn <sparco2405@gmail.com>
2020-06-08 18:30:30 +0300

    add reducers for state & refactoring

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem'
 import Message from './Messages/Message'
-import {sendMessageCreator, updateNewMessageCreator} from '../../redux/State';
+import {sendMessageCreator, updateNewMessageCreator} from '../../redux/dialogs_reducer';
 
 
 
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,7 +1,7 @@
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
-const SEND_MESSAGE = 'SEND-MESSAGE';
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+import profileReducer from "./profile_reducer";
+import dialogsReducer from "./dialogs_reducer";
+import sidebarReducer from "./sidebar_reducer";
+
 
 let store = {
 
@@ -30,7 +30,8 @@ let store = {
                 {id: 5, message: 'what?'}
             ],
             newMessageBody: ''
-        }
+        },
+        sidebar: {}
     },
     
     _callSubscriber() {
@@ -46,45 +47,14 @@ let store = {
     },
 
     dispatch(action) {
+
+        this._state.profilePage = profileReducer(this._state.profilePage, action);
+        this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+        this._state.sidebar = sidebarReducer(this._state.sidebar, action);
+        this._callSubscriber(this._state);
         
-        if (action.type === ADD_POST) {
-            let newPost = {
-                id: 4,
-                message: this._state.profilePage.newPostText,
-                likesCount: 0
-            };
-            this._state.profilePage.posts.push(newPost);
-            this._state.profilePage.newPostText = '';  // занулили зачистили текстарею
-            this._callSubscriber(this._state);
-        }
-        else if (action.type === UPDATE_NEW_POST_TEXT) {
-            this._state.profilePage.newPostText = action.newText;
-            this._callSubscriber(this._state);
-        }
-        else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-            this._state.dialogsPage.newMessageBody = action.body;   
-            this._callSubscriber(this._state);
-        }
-        else if (action.type === SEND_MESSAGE){
-            let body = this._state.dialogsPage.newMessageBody;
-            this._state.dialogsPage.newMessageBody = '';
-            this._state.dialogsPage.messages.push({id: 6, message: body})
-            this._callSubscriber(this._state);
-        }
     }
 }
-
-export const addPostActionCreator = () =>  ({type: ADD_POST}) //можно return опустить
-  
-export const updateNewPostTextActionCreator = (text) => {
-    return {type: UPDATE_NEW_POST_TEXT, newText: text};
-}
-export const sendMessageCreator = () => {
-    return {type: SEND_MESSAGE};//экспортируем условия обьекта {}
-}
-export const updateNewMessageCreator = (body) => {
-    return {type: UPDATE_NEW_MESSAGE_BODY, body: body};
-  }
 
 export default store;
 window.store = store;
